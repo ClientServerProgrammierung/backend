27c2de0e52bd6d6e9dabc220cd78f8babf772ec5
INF/ibsc4759 <ibrahim.schech-saher@stud.hs-flensburg.de>
2022-01-04 23:06:22 +0100

    Implement KostenArtenGateway and updatebenutzer in BenutzerGateway

--- src/main/java/Controller/JsfTest.java
+++ src/main/java/Controller/JsfTest.java
@@ -1,9 +1,11 @@
 package Controller;
 
 import model.Fahrrad;
+import model.KostenArten;
 
 import java.util.List;
 import gateway.FahrradGateway;
+import gateway.KostenArtenGateway;
 
 
 public class JsfTest {
@@ -12,23 +14,28 @@ public class JsfTest {
 		
 		
 
-		FahrradGateway gateway = new FahrradGateway();
-		Fahrrad fahrrad = new Fahrrad();
-		fahrrad.setRahmennummer("1293509");
-		fahrrad.setMarke("Serious");
-		fahrrad.setModel("Rockville 27.5\" schwarz/grau");
+//		FahrradGateway gateway = new FahrradGateway();
+//		Fahrrad fahrrad = new Fahrrad();
+//		fahrrad.setRahmennummer("1293509");
+//		fahrrad.setMarke("Serious");
+//		fahrrad.setModel("Rockville 27.5\" schwarz/grau");
+//		
+//		gateway.insertFahrrad(fahrrad);
+//		Fahrrad f = new Fahrrad();
+//		f.setRahmennummer("1534045");
+//		f.setMarke("GIANT");
+//		f.setModel("Talon 4 schwarz");
+//		
+//		gateway.insertFahrrad(fahrrad, f);
+//		
+//		
+//		List<Fahrrad> list = gateway.getAll();
+//		list.forEach(ff -> System.out.println(ff.getModel()));
 		
-		gateway.insertFahrrad(fahrrad);
-		Fahrrad f = new Fahrrad();
-		f.setRahmennummer("1534045");
-		f.setMarke("GIANT");
-		f.setModel("Talon 4 schwarz");
+		KostenArtenGateway gateway = new KostenArtenGateway();
+		List<KostenArten> list =gateway.getAllKostenArten();
+		list.forEach(ff -> System.out.println(ff.getBeschreibung()));
 		
-		gateway.insertFahrrad(fahrrad, f);
-		
-		
-		List<Fahrrad> list = gateway.getAll();
-		list.forEach(ff -> System.out.println(ff.getModel()));
 	}
 	
 }
