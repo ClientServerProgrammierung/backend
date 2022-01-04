dca61aae4ab8a89452fc85506f8bcf1cd3f54fc7
Lukas Nolde <lukasnolde@Lukass-MacBook-Pro.fritz.box>
2022-01-04 19:42:24 +0100

    added stuff to Fahrrad

--- src/main/java/frontendVendor/VendorFahrradAnlegen.java
+++ src/main/java/frontendVendor/VendorFahrradAnlegen.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import gateway.FahrradGateway;
+import gateway.KostenGateway;
 import model.Fahrrad;
 import model.Kosten;
 import model.KostenArten;
@@ -29,27 +30,34 @@ public class VendorFahrradAnlegen extends HttpServlet {
 	    String rahmennummer = request.getParameter("rahmennummer");
 	    String marke = request.getParameter("marke");
 	    String model = request.getParameter("model");
-	    double anschaffungspreis = request.getParameter("anschaffungspreis");
-	    String mietpreis = request.getParameter("mietpreis");
+	    double anschaffungspreis = Double.parseDouble(request.getParameter("anschaffungspreis"));
+	    double mietpreis = Double.parseDouble(request.getParameter("mietpreis"));
 	    
-	    // Fahrrad - Model
+	    // Models
 	    Fahrrad fahrrad = new Fahrrad(rahmennummer, marke, model);
+	    Kosten aPreis = new Kosten();
+	    Kosten mPreis = new Kosten();
 	    
-	    Kosten asPreis = new Kosten();
-	    asPreis.setHoehe(anschaffungspreis);
-	    asPreis.setKostenArt();
+	    KostenArten artAPreis = new KostenArten();
+	    KostenArten artMPreis = new KostenArten();
 	    
-        Kosten mPreis = new Kosten();
-        
-        
 	    
-	    // Gateway
+	    // set Anschaffungspreis - //TODO -- kosten art ist momentan als int implementiert - soll das noch geändert werden
+	    aPreis.setHoehe(anschaffungspreis);
+	    aPreis.setKostenArt(1);
+	    
+	    // set Mietpreis
+        mPreis.setHoehe(mietpreis);
+        mPreis.setKostenArt(1);
+
+	    // Gateways
 	    FahrradGateway fahrradGateway = new FahrradGateway();
+	    KostenGateway kostenGateway = new KostenGateway();
 	    
-	    // fahrrad in Datenbank hinzufügen
+	    // Daten in Datenbank einfügen
 	    fahrradGateway.insertFahrrad(fahrrad);
+	    kostenGateway.insertKosten(aPreis, mPreis);
 	    
 	    
 	}
-
 }
