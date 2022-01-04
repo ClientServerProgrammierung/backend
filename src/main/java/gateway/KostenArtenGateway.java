5630aef1188006d3bb21818ab53626cd41d223af
alka7924 <alexander.kaiser@stud.hs-flensburg.de>
2022-01-04 19:31:06 +0100

    Added Gateway for kosten_arten

---
+++ src/main/java/gateway/KostenArtenGateway.java
@@ -0,0 +1,40 @@
+package gateway;
+
+import java.util.List;
+import javax.persistence.TypedQuery;
+
+import model.Kosten;
+import model.KostenArten;
+
+public class KostenArtenGateway extends Gateway {
+
+	public List<KostenArten> getAll() {
+		TypedQuery<KostenArten> query = manager.createQuery("SELECT k FROM kosten_arten k", KostenArten.class);
+		
+		List<KostenArten> liste = query.getResultList();
+		return liste;
+	}
+	
+	public KostenArten getById(Integer id) throws Exception {
+		TypedQuery<KostenArten> query = manager.createQuery("SELECT k FROM kosten_arten k WHERE k.id= ?1", KostenArten.class);
+		query.setParameter(1, id);
+		
+		return query.getResultList().stream()
+				.findAny()
+				.orElseThrow(() -> new Exception("Not exactly 1 found."));
+	}
+	
+	public void insertKostenArten(KostenArten ... arten) {
+		manager.getTransaction().begin();
+		for (KostenArten k : arten) {
+			manager.persist(k);
+		}
+		manager.getTransaction().commit();
+	}
+
+	public void deleteKosten(KostenArten ... arten) {
+		for (KostenArten k : arten) {
+			manager.remove(k);
+		}
+	}
+}
