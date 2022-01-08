441c675de8870d856d89a8033f93b5c70aa900e1
alka7924 <alexander.kaiser@stud.hs-flensburg.de>
2022-01-08 13:01:42 +0100

    Fix JSFTest

--- src/main/java/Controller/JsfTest.java
+++ src/main/java/Controller/JsfTest.java
@@ -2,6 +2,7 @@ package Controller;
 
 import model.Benutzer;
 import model.Fahrrad;
+import model.Kosten;
 import model.KostenArten;
 
 import java.time.LocalTime;
@@ -11,6 +12,7 @@ import java.util.Date;
 import gateway.BenutzerGateway;
 import gateway.FahrradGateway;
 import gateway.KostenArtenGateway;
+import gateway.KostenGateway;
 import gateway.LoginGateway;
 
 
@@ -18,60 +20,11 @@ public class JsfTest {
 	//TODO: delete before deadline
 	@SuppressWarnings("deprecation")
 	public static void main (String[] args) {
-		
 
-		
-		/*
 
-//		FahrradGateway gateway = new FahrradGateway();
-//		Fahrrad fahrrad = new Fahrrad();
-//		fahrrad.setRahmennummer("1293509");
-//		fahrrad.setMarke("Serious");
-//		fahrrad.setModel("Rockville 27.5\" schwarz/grau");
-//		
-//		gateway.insertFahrrad(fahrrad);
-//		Fahrrad f = new Fahrrad();
-//		f.setRahmennummer("1534045");
-//		f.setMarke("GIANT");
-//		f.setModel("Talon 4 schwarz");
-//		
-//		gateway.insertFahrrad(fahrrad, f);
-//		
-//		
-//		List<Fahrrad> list = gateway.getAll();
-//		list.forEach(ff -> System.out.println(ff.getModel()));
+		KostenGateway gw = new KostenGateway();
+		List<Kosten> kosten = gw.getKostenByRahmennummer("123132");
 		
-/*
-		 LoginGateway gateway = new LoginGateway();
-		Benutzer benutzer = new Benutzer();
-		benutzer.setVorname("nn");
-		benutzer.setNachname("mm");
-		benutzer.setEmail("qq@ss.de");
-		benutzer.setPassword("qq");
-		benutzer.setGeburtstdatum(new Date(1992, 1,1));
- 		benutzer.setAdresse("irr");
-	// 	gateway.insertBenutzer(benutzer);
-//		List<Benutzer> list =gateway.getAllBenutzer();
-    //	list.forEach(ff -> System.out.println(ff.getPassword()));
-		System.out.println(gateway.IsLogged(benutzer));
-		
-//		BenutzerGateway gateway = new BenutzerGateway();
-//		Benutzer benutzer = gateway.getBenutzerByEmail("ib@gm.de");
-//		benutzer.setVorname("bra");
-//		gateway.updateBenutzer(benutzer);
-//		List<Benutzer> list =gateway.getAllBenutzer();
-//		list.forEach(ff -> System.out.println(ff.getVorname()));
-<<<<<<< HEAD
-		
-*/
-		
-		FahrradGateway gw = new FahrradGateway();
-		gw.deleteFahrrad(gw.getAll().get(0));
-     
-=======
- 
- */
->>>>>>> devVendor
 	}
 	
 }
